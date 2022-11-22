a2d8b9e4db81fd63a3989d20f6a242e69e22b26c
Carlos Eduardo <102683309+carloseduardo-silva@users.noreply.github.com>
2022-11-22 15:58:44 -0300

    onresize tryng

--- pages/script.js
+++ pages/script.js
@@ -5,3 +5,18 @@ function menuopen(){
     const menu = document.querySelector("#menu")
     menu.classList.toggle("open")
 }
+ 
+function closemenu() {
+    if(innerWidth > 800){
+        const menu = document.querySelector("#menu")
+        menu.classList.remove()
+        menu.classList.add('hide')
+
+}
+}
+
+
+
+window.addEventListener('onresize', closemenu())
+
+    
