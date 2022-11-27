6f2c83f4c99befacbacedcd7324065dcb61c5a35
Carlos Eduardo <102683309+carloseduardo-silva@users.noreply.github.com>
2022-11-27 14:59:42 -0200

    site pronto

--- pages/script.js
+++ pages/script.js
@@ -5,6 +5,13 @@ function menuopen(){
     const menu = document.querySelector("#menu")
     menu.classList.toggle("open")
 }
+
+function biblioOpen(){
+    
+    const biblioReal = document.querySelector('#Bibliografia')
+    biblioReal.classList.toggle("aberto")
+
+}
  
 function closemenu() {
     if(innerWidth > 800){
